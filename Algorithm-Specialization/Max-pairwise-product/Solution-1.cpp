9a413a6d5dad0d8b2e67bfa8743d3cd0558f7821
Koushik Saha <54022602+Koushikask@users.noreply.github.com>
2021-08-07 15:31:46 +0530

    ended the game of maxPairwiseProduct i guess

--- Algorithm-Specialization/Max-pairwise-product/Solution-1.cpp
+++ Algorithm-Specialization/Max-pairwise-product/Solution-1.cpp
@@ -1,26 +1,75 @@
+// Max Pairwise Product Solution
 #include <iostream>
 #include <cmath>
 #include <vector>
 
-using namespace std;
+using std::cin;
+using std::cout;
+using std::vector;
 
-int getNumber(int number, int min_input, int max_input);
-long long maxPairwiseProduct_slow(const vector<int> arr);
-long long maxPairwiseProduct_fast_wrong(const vector<int> arr);
+int getNumber(const int &min, const int &max);
+long long maxPairwiseProduct_slow(const vector<int> &arr);
+long long maxPairwiseProduct_fast_wrong(const vector<int> &arr);
 long long maxPairwiseProduct_fast(const vector<int> &values);
-int bubbleSort(vector<int> &arr);
+void bubbleSort(vector<int> &arr);
 
-int getNumber(int number, int min_input, int max_input)
+int main()
 {
-    do
+    int no_of_sequence{}, min_input{}, max_input{};
+    max_input = std::pow(10, 5);
+
+    // Take number of sequence input
+    no_of_sequence = getNumber(min_input + 2, 2 * max_input);
+
+    vector<int> arr(no_of_sequence);
+    // Take vector Input
+    for (auto &value : arr)
+        value = getNumber(min_input, max_input);
+
+    // Way of using multiply values with each other
+    long long product_1{maxPairwiseProduct_slow(arr)};
+
+    // Way of using just find 2 max Number to multiply --Fast but wrong
+    long long product_2{maxPairwiseProduct_fast_wrong(arr)};
+
+    // Way of using just find 2 max Number to multiply --Fast
+    long long product_3{maxPairwiseProduct_fast(arr)};
+
+    // Way of using Bubble Sort
+    bubbleSort(arr);
+    size_t last{arr.size() - 1};
+    long long product_4{static_cast<long long>(arr.at(last)) * arr.at(last - 1)};
+
+    cout << " Different Max Pairwise product functions:"
+         << "\n\tslow version - " << product_1
+         << "\n\tFast Wrong version - " << product_2
+         << "\n\tFast version - " << product_3
+         << "\n\tBubble Sort version - " << product_4 << "\n";
+
+    cout << " 🎷 They all are same its " << std::boolalpha << (product_1 == product_2 && product_2 == product_3 && product_3 == product_4) << "\n";
+
+    // Show Sorted Arr list
+    cout << "\nBubble Sorted Array: \n";
+    for (auto val : arr)
+        cout << "\t" << val;
+
+    return 0;
+}
+
+int getNumber(const int &min, const int &max)
+{
+    int number{};
+
+    while (true)
     {
         cin >> number;
-    } while (number > max_input || number < min_input);
-
+        if (number > min && number < max)
+            break;
+    }
     return number;
 }
 
-long long maxPairwiseProduct_slow(const vector<int> arr)
+long long maxPairwiseProduct_slow(const vector<int> &arr)
 {
     long long product{};
     for (int i{}; i < arr.size() - 1; ++i)
@@ -30,7 +79,7 @@ long long maxPairwiseProduct_slow(const vector<int> arr)
     return product;
 }
 
-long long maxPairwiseProduct_fast_wrong(const vector<int> arr)
+long long maxPairwiseProduct_fast_wrong(const vector<int> &arr)
 {
     int maxIndex_1{-1};
 
@@ -59,12 +108,12 @@ long long maxPairwiseProduct_fast(const vector<int> &arr)
         if (j != max_index1 && (max_index2 == -1 || arr.at(j) > arr.at(max_index2)))
             max_index2 = j;
 
-    cout << " The Max 2 Values are " << arr.at(max_index2) << " and " << arr.at(max_index1) << endl;
+    cout << " The Max 2 Values are " << arr.at(max_index2) << " and " << arr.at(max_index1) << "\n";
 
     return (static_cast<long long>(arr.at(max_index1)) * arr.at(max_index2));
 }
 
-int bubbleSort(vector<int> &arr)
+void bubbleSort(vector<int> &arr)
 {
     for (int i{}; i < arr.size() - 1; ++i)
         for (int j{}; j < arr.size() - i - 1; ++j)
@@ -74,49 +123,4 @@ int bubbleSort(vector<int> &arr)
                 arr.at(j) = arr.at(j + 1) - arr.at(j);
                 arr.at(j + 1) -= arr.at(j);
             }
-
-    return 0;
-}
-
-int main()
-{
-    int no_of_sequence{}, min_input{}, max_input{};
-
-    max_input = pow(10, 5);
-    // Take number of sequence input
-    no_of_sequence = getNumber(no_of_sequence, min_input + 2, 2 * max_input);
-
-    vector<int> arr(no_of_sequence);
-    // Take Array Input
-    for (size_t i{}; i < arr.size(); ++i)
-        arr.at(i) = getNumber(arr.at(i), min_input, max_input);
-
-    // Way of using multiply values with each other
-    long long product_1{maxPairwiseProduct_slow(arr)};
-
-    // Way of using just find 2 max Number to multiply --Fast but wrong
-    long long product_2{maxPairwiseProduct_fast_wrong(arr)};
-
-    // Way of using just find 2 max Number to multiply --Fast
-    long long product_3{maxPairwiseProduct_fast(arr)};
-
-    // Way of using Bubble Sort
-    bubbleSort(arr);
-    size_t last{arr.size() - 1};
-    long long product_4{static_cast<long long>(arr.at(last)) * arr.at(last - 1)};
-
-    cout << " Different Max Pairwise product functions:"
-         << "\n\tslow version - " << product_1
-         << "\n\tFast Wrong version - " << product_2
-         << "\n\tFast version - " << product_3
-         << "\n\tBubble Sort version - " << product_4 << "\n";
-
-    cout << " 🎷 They all are same its " << std::boolalpha << (product_1 == product_2 && product_2 == product_3 && product_3 == product_4) << "\n";
-
-    // Show Sorted Arr list
-    // cout << "\nBubble Sorted Array: " << endl;
-    // for (auto val : arr)
-    //     cout << "\t" << val;
-
-    return 0;
 }
--- Algorithm-Specialization/Max-pairwise-product/Solution-stress_test.cpp
+++ Algorithm-Specialization/Max-pairwise-product/Solution-stress_test.cpp
@@ -1,53 +1,119 @@
+// Max Pairwise Product Solution
 #include <iostream>
+#include <cmath>
 #include <vector>
+#include <cctype>
 
 using std::cin;
 using std::cout;
-using std::endl;
 using std::vector;
 
-// Function Prototypes
-void take_input(int &num);
-long long products_of_max(const vector<int> &values);
+int getNumber(const int &min, const int &max);
+long long maxPairwiseProduct_slow(const vector<int> &arr);
+long long maxPairwiseProduct_fast_wrong(const vector<int> &arr);
+long long maxPairwiseProduct_fast(const vector<int> &values);
+void bubbleSort(vector<int> &arr);
 
 int main()
 {
-    int arr_size{};
+    int no_of_sequence{}, min_input{}, max_input{};
 
-    cout << "Enter the number of elements you want to add " << endl;
-    take_input(arr_size);
+    // Stress test implementation
+    while (true)
+    {
+        no_of_sequence = std::rand() % 1000 + 2;
+        vector<int> arr(no_of_sequence);
 
-    vector<int> numbers(arr_size);
-    for (auto &num : numbers)
-        cin >> num;
+        cout << "=> " << no_of_sequence << "\n";
 
-    long long product{};
-    product = products_of_max(numbers);
-    cout << " And, The maxpair product value is :" << product << endl;
+        for (auto &value : arr)
+        {
+            value = std::rand() % 100000 + 1;
+            cout << value << "  ";
+        }
+        cout << "\n";
+
+        // MaxPairwise multiply --Slow
+        long long product_1{maxPairwiseProduct_slow(arr)};
+
+        // MaxPairwise multiply --Fast
+        long long product_2{maxPairwiseProduct_fast(arr)};
+
+        // MaxPairwise multiply --Fast(Wrong Sometime)
+        // long long product_3{maxPairwiseProduct_fast_wrong(arr)};
+
+        // Bubble Sort
+        bubbleSort(arr);
+        size_t last{arr.size() - 1};
+        long long product_4{static_cast<long long>(arr.at(last)) * arr.at(last - 1)};
+
+        if (product_1 == product_2 && product_2 == product_4)
+            cout << "OK!\n";
+        else
+        {
+            cout << "Wrong " << product_1 << "\t" << product_2 << "\t" << product_4 << "\n";
+            break;
+        }
+
+        // cout << " Different Max Pairwise product functions:"
+        //      << "\n\tslow version - " << product_1
+        //      << "\n\tFast version - " << product_2
+        //      << "\n\tBubble Sort version - " << product_3 << "\n";
+    }
 
     return 0;
 }
 
-void take_input(int &num)
+long long maxPairwiseProduct_slow(const vector<int> &arr)
 {
-    do
-        cin >> num;
-    while (num < 2);
+    long long product{};
+    for (int i{}; i < arr.size() - 1; ++i)
+        for (int j{i + 1}; j < arr.size(); ++j)
+            if (static_cast<long long>(arr.at(i)) * arr.at(j) > product)
+                product = static_cast<long long>(arr.at(i)) * arr.at(j);
+    return product;
 }
 
-long long products_of_max(const vector<int> &values)
+// long long maxPairwiseProduct_fast_wrong(const vector<int> &arr)
+// {
+//     int maxIndex_1{-1};
+
+//     for (int i{}; i < arr.size(); ++i)
+//         if (maxIndex_1 == -1 || arr.at(i) > arr.at(maxIndex_1))
+//             maxIndex_1 = i;
+
+//     int maxIndex_2{-1};
+//     for (int j{}; j < arr.size(); ++j)
+//         if (arr.at(j) != arr.at(maxIndex_1) && (maxIndex_2 == -1 || arr.at(j) > arr.at(maxIndex_2)))
+//             maxIndex_2 = j;
+
+//     // cout << arr.at(maxIndex_1) <<" - " << arr.at(maxIndex_2);
+//     return (static_cast<long long>(arr.at(maxIndex_1)) * arr.at(maxIndex_2));
+// }
+
+long long maxPairwiseProduct_fast(const vector<int> &arr)
 {
     int max_index1{-1};
-    for (size_t i{}; i < values.size(); ++i)
-        if (max_index1 == -1 || values.at(i) > values.at(max_index1))
+    for (size_t i{}; i < arr.size(); ++i)
+        if (max_index1 == -1 || arr.at(i) > arr.at(max_index1))
             max_index1 = i;
 
     int max_index2{-1};
-    for (size_t j{}; j < values.size(); ++j)
-        if (j != max_index1 && (max_index2 == -1 || values.at(j) > values.at(max_index2)))
+    for (size_t j{}; j < arr.size(); ++j)
+        if (j != max_index1 && (max_index2 == -1 || arr.at(j) > arr.at(max_index2)))
             max_index2 = j;
 
-    cout << " The Max 2 Values are " << values.at(max_index2) << " and " << values.at(max_index1) << endl;
+    return (static_cast<long long>(arr.at(max_index1)) * arr.at(max_index2));
+}
 
-    return (static_cast<long long>(values.at(max_index1)) * values.at(max_index2));
+void bubbleSort(vector<int> &arr)
+{
+    for (int i{}; i < arr.size() - 1; ++i)
+        for (int j{}; j < arr.size() - i - 1; ++j)
+            if (arr.at(j) > arr.at(j + 1))
+            {
+                arr.at(j + 1) += arr.at(j);
+                arr.at(j) = arr.at(j + 1) - arr.at(j);
+                arr.at(j + 1) -= arr.at(j);
+            }
 }
