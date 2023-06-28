0bb91467d15601eaa7336c8dc796e6cd3156743b
Koushikon <54022602+Koushikon@users.noreply.github.com>
2023-06-29 00:27:15 +0530

    Edit: change the implementation

--- 03.CPP_Structure/AreaofRoom.cpp
+++ 03.CPP_Structure/AreaofRoom.cpp
@@ -1,16 +1,67 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+bool FLAG_REPEAT = false;
+
+char Take_Input()
+{
+    if (FLAG_REPEAT == false)
+    {
+        FLAG_REPEAT = true;
+        return 'R';
+    }
+
+    char n = 0;
+    cout << "Want to continue? - ";
+    cin >> n;
+
+    return n;
+}
+
+int Take_Input(string message)
+{
+    int n = 0;
+    cout << message;
+    cin >> n;
+
+    return n;
+}
+
+int Cal_Area(int height = 1, int width = 1)
+{
+    return height * width;
+}
+
 int main()
 {
-    cout << "Enter the Room width: ";
-    int room_width {0};
-    cin >> room_width;
+    int room_width{1}, room_height{1};
 
+    while (tolower(Take_Input()) != 'n')
+    {
+        room_width = Take_Input("Enter the Room width: ");
+        room_height = Take_Input("Enter the Room height: ");
+
+        cout << "The area of the room is " << Cal_Area(room_width, room_height) << " squire feet" << endl;
+    }
+}
+
+/**
+ * Or, Run this program with single Method
+ * In the following:
+ */
+
+/*
+int main()
+{
+    int room_width{1}, room_height{1};
+
+    cout << "Enter the Room width: ";
+    cin >> room_width;
     cout << "Enter the Room height: ";
-    int room_height {0};
     cin >> room_height;
 
-    cout << "The area of the room is " << room_width * room_height << " squire feet" << endl;
+    cout << "The area of the room is " << Cal_Area(room_width, room_height) << " squire feet" << endl;
 }
+*/
